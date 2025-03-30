3407db76c401a9349e41782a264c9832884ed49d
Ashish Sharma <ashish992005@gmail.com>
2025-03-30 20:39:19 +0530

    [feat] Implement API endpoints for Menu items and Bookings - Added `rest_framework` to the installed apps. - Created a router for booking and registered the BookingViewSet with it. Included the router URLs in the main project's URL configuration. - Defined API views for menu items (list/create and retrieve/update/destroy) using generics. - Defined a viewset for bookings using ModelViewSet. - Updated the restaurant app's URL configuration to include the new API endpoints for menu items. - Removed the index view and its corresponding URL pattern.

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from rest_framework import response,generics
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
 
 
-def index(request):
-    return render(request, 'index.html', {})
+
+
